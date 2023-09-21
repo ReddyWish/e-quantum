3540d74bfa84fa270052b137d02cf249bb043ce9
Ilya <ilya.shuvatov1@gmail.com>
2023-09-21 14:14:54 +0200

    auth and register on backend implemented

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 const port = process.env.PORT || 5000;
 
 connectDB();
@@ -12,8 +14,10 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 
 
