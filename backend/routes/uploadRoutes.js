356d90ffa7d038c05c404d03e8bad1517f1d7b11
Ilya <ilya.shuvatov1@gmail.com>
2023-09-23 11:34:48 +0200

    fixed upload bug

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -4,12 +4,15 @@ import multer from 'multer';
 const router = express.Router();
 
 const storage = multer.diskStorage({
-  destination(req, res, cb) {
-    cb(null, 'uploads/')
+  destination(req, file, cb) {
+    cb(null, 'uploads/');
   },
   filename(req, file, cb) {
-    cb(null, `${file.fieldname}-${Date.now()}${path.extname((file.originalname))}`);
-  }
+    cb(
+      null,
+      `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
+    );
+  },
 });
 
 function fileFilter(req, file, cb) {
